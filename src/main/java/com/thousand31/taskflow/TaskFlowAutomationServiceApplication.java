c57d2ec258699439bd81d13dc8c1238289416622
Shaiful Islam Palash <kuvic16@gmail.com>
2025-11-30 20:24:39 +0600

    Initial Spring Boot project setup

---
+++ src/main/java/com/thousand31/taskflow/TaskFlowAutomationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thousand31.taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowAutomationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFlowAutomationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thousand31/taskflow/TaskFlowAutomationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thousand31.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowAutomationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
