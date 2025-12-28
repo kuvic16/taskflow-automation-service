353724bcb97ef79979117b229b803f183fdd6c98
Shaiful Islam Palash <kuvic16@gmail.com>
2025-12-28 22:19:27 +0600

    login and signup completed

--- src/main/java/com/thousand31/taskflow/controller/AuthController.java
+++ src/main/java/com/thousand31/taskflow/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.thousand31.taskflow.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @PostMapping("/signup")
     public ResponseEntity<JwtResponse> register(@Valid @RequestBody SignupRequest request){
         JwtResponse response = authService.register(request);
         return ResponseEntity.ok(response);
     }
 
+    @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginRequest){
         JwtResponse response = authService.login(loginRequest);
         return ResponseEntity.ok(response);
