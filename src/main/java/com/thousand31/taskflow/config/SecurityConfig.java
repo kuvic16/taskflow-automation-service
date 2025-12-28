7128d4de9a017fa4181d7c91870a30fd8d3ba60f
Shaiful Islam Palash <kuvic16@gmail.com>
2025-12-28 17:55:59 +0600

    project practising is in progress

---
+++ src/main/java/com/thousand31/taskflow/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.thousand31.taskflow.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csf -> csf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
+                        "/api/auth/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html"
+                ).permitAll().anyRequest().authenticated())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/thousand31/taskflow/service/AuthService.java
@@ -0,0 +1,60 @@
+package com.thousand31.taskflow.service;
+
+import com.thousand31.taskflow.config.JwtUtil;
+import com.thousand31.taskflow.dto.auth.JwtResponse;
+import com.thousand31.taskflow.dto.auth.SignupRequest;
+import com.thousand31.taskflow.model.Role;
+import com.thousand31.taskflow.model.User;
+import com.thousand31.taskflow.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public JwtResponse register(SignupRequest request){
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new IllegalArgumentException("Email is already in use");
+        }
+        User user = User.builder()
+                .fullName(request.getFullName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .roles(Set.of(Role.ROLE_USER))
+                .build();
+        userRepository.save(user);
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+        String token = jwtUtil.generateToken(userDetails);
+
+        return buildJwtResponse(user, token);
+    }
+
+    private JwtResponse buildJwtResponse(User user, String token){
+        return JwtResponse.builder()
+                .accessToken(token)
+                .userId(user.getId())
+                .fullName(user.getFullName())
+                .email(user.getEmail())
+                .roles(
+                        user.getRoles().stream()
+                                .map(Enum::name)
+                                .collect(Collectors.toSet())
+                )
+                .build();
+    }
+
+}
