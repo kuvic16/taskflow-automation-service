a26de9d6f24f04aec6602247f4c3087fa0b04482
Shaiful Islam Palash <kuvic16@gmail.com>
2026-01-10 16:00:55 +0600

    task service completed

--- src/main/java/com/thousand31/taskflow/service/TaskService.java
+++ src/main/java/com/thousand31/taskflow/service/TaskService.java
@@ -1,13 +1,17 @@
-package com.thousand31.taskflow.repository;
+package com.thousand31.taskflow.service;
 
 import com.thousand31.taskflow.dto.task.TaskRequest;
 import com.thousand31.taskflow.dto.task.TaskResponse;
 import com.thousand31.taskflow.model.Task;
 import com.thousand31.taskflow.model.TaskStatus;
 import com.thousand31.taskflow.model.User;
+import com.thousand31.taskflow.repository.TaskRepository;
+import com.thousand31.taskflow.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,6 +38,43 @@ public class TaskService {
                 .build();
         Task saved = taskRepository.save(task);
         return mapToResponse(saved);
+    }
+
+    public List<TaskResponse> getAllTasks() {
+        User user = getCurrentUser();
+        return taskRepository.findByUser(user)
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    public TaskResponse getTaskById(Long id) {
+        User user = getCurrentUser();
+        Task task = taskRepository.findByIdAndUser(id, user)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+        return mapToResponse(task);
+    }
+
+    public TaskResponse updateTask(Long id, TaskRequest request){
+        User user = getCurrentUser();
+        Task task = taskRepository.findByIdAndUser(id, user)
+                .orElseThrow(()-> new RuntimeException("Task not found"));
+
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+        task.setDueDate(request.getDueDate());
+        task.setReminderTime(request.getReminderDate());
+
+        return mapToResponse(taskRepository.save(task));
+    }
+
+    public void deleteTask(Long id){
+        User user = getCurrentUser();
+
+        Task task = taskRepository.findByIdAndUser(id, user)
+                .orElseThrow(()->new RuntimeException("Task not found"));
+
+        taskRepository.delete(task);
     }
 
     private TaskResponse mapToResponse(Task task){
